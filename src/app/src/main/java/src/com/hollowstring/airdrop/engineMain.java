d096aac584bb4c6f372e45f1ff4ff62e8d392c39
MD Mushfique Farhan Nabir <82253045+Nabir14@users.noreply.github.com>
2025-09-23 18:59:24 +0000

    yeah redo again

--- src/app/src/main/java/src/com/hollowstring/airdrop/engineMain.java
+++
@@ -1,9 +0,0 @@
-package com.hollowstring.airdrop;
-
-import org.lwjgl.Version;
-
-public class engineMain {
-    public static void main(String[] args) {
-        System.out.println(Version.BuildType.getVersionString());
-    }
-}

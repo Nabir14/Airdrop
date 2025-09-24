e2ce63feadf2c0895af767536ce058b19287586d
MD Mushfique Farhan Nabir <82253045+Nabir14@users.noreply.github.com>
2025-09-24 18:51:12 +0000

    it works finally YES!

---
+++ src/app/src/main/java/src/com/hollowstring/airengine/airengine.java
@@ -0,0 +1,9 @@
+package src.com.hollowstring.airengine;
+
+import org.lwjgl.*;
+
+public class airengine {
+    public static void main(String[] args) {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+    }
+}

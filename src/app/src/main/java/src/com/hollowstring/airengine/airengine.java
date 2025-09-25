583e2736fe06ed76535b5980080d0dbb5821282a
MD Mushfique Farhan Nabir <82253045+Nabir14@users.noreply.github.com>
2025-09-25 09:16:15 +0000

    t

--- src/app/src/main/java/src/com/hollowstring/airengine/airengine.java
+++ src/app/src/main/java/src/com/hollowstring/airengine/airengine.java
@@ -3,15 +3,10 @@ package src.com.hollowstring.airengine;
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengles.*;
-import org.lwjgl.system.MemoryUtil;
-
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-
-import org.joml.*;
 
 public class airengine {
     public static long window;
+
     public static void init(){
         GLFW.glfwInit();
         GLFW.glfwDefaultWindowHints();
@@ -21,14 +16,13 @@ public class airengine {
         GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
         GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GLES32.GL_TRUE);
     }
+
     public static void createWindow(int width, int height, String title){
-        // String dosen't terminate properly 
-        Charset utf8 = Charset.forName("UTF-8");
-        ByteBuffer t = utf8.encode(title);
-        window = GLFW.glfwCreateWindow(width, height, t, MemoryUtil.NULL, MemoryUtil.NULL);
+        window = GLFW.glfwCreateWindow(width, height, title, 0, 0);
         GLFW.glfwMakeContextCurrent(window);
         GLES32.glViewport(0, 0, width, height);
     }
+
     public static void loopDefault(){
         GLFW.glfwSwapBuffers(window);
         GLFW.glfwPollEvents();

90523b87fdbd667c7359de4109498a0e8d47692e
MD Mushfique Farhan Nabir <82253045+Nabir14@users.noreply.github.com>
2025-09-25 08:39:11 +0000

    window

---
+++ src/app/src/main/java/airdrop.java
@@ -0,0 +1,9 @@
+import src.com.hollowstring.airengine.*;
+
+public class airdrop {
+    public static void main(String[] args) {
+        airengine.init();
+        airengine.createWindow(800, 600, "Airdrop");
+        airengine.close();
+    }
+}
--- src/app/src/main/java/src/com/hollowstring/airengine/airengine.java
+++ src/app/src/main/java/src/com/hollowstring/airengine/airengine.java
@@ -1,9 +1,40 @@
 package src.com.hollowstring.airengine;
 
 import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengles.*;
+import org.lwjgl.system.MemoryUtil;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+import org.joml.*;
 
 public class airengine {
-    public static void main(String[] args) {
-        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+    public static long window;
+    public static void init(){
+        GLFW.glfwInit();
+        GLFW.glfwDefaultWindowHints();
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 2);
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GLES32.GL_TRUE);
+    }
+    public static void createWindow(int width, int height, String title){
+        // String dosen't terminate properly 
+        Charset utf8 = Charset.forName("UTF-8");
+        ByteBuffer t = utf8.encode(title);
+        window = GLFW.glfwCreateWindow(width, height, t, MemoryUtil.NULL, MemoryUtil.NULL);
+        GLFW.glfwMakeContextCurrent(window);
+        GLES32.glViewport(0, 0, width, height);
+    }
+    public static void loopDefault(){
+        GLFW.glfwSwapBuffers(window);
+        GLFW.glfwPollEvents();
+    }
+    public static void close(){
+        GLFW.glfwWindowShouldClose(window);
+        GLFW.glfwTerminate();
     }
 }

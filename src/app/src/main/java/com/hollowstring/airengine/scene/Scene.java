54aa4e33e9fcacbbaf984c4ddc6289df536f06c9
MD Mushfique Farhan Nabir <82253045+Nabir14@users.noreply.github.com>
2025-09-26 06:54:07 +0000

    add scene

--- src/app/src/main/java/com/hollowstring/airengine/scene/Scene.java
+++ src/app/src/main/java/com/hollowstring/airengine/scene/Scene.java
@@ -1,5 +1,43 @@
 package com.hollowstring.airengine.scene;
 
+import org.lwjgl.opengl.*;
+
+import com.hollowstring.airengine.camera.Camera;
+import com.hollowstring.airengine.object.Object;
+
 public class Scene {
+    private Camera activeCamera;
+    private Object[] objectPoll;
+    private int VBO, VAO;
+
+    public Scene(){
+        GL.createCapabilities();
+        VBO = GL15.glGenBuffers();
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, VBO);
+    }
+
+    public Camera getActiveCamera() {
+        return activeCamera;
+    }
+
+    public void setActiveCamera(Camera activeCamera) {
+        this.activeCamera = activeCamera;
+    }
+
+    public void appendObject(Object obj){
+        if(objectPoll == null) {
+            objectPoll = new Object[0];
+            objectPoll[0] = obj;
+
+        }else{
+            objectPoll[objectPoll.length + 1] = obj;
+        }
+    }
+
+    public void render(){
+        for(int i = 0; i < objectPoll.length; i++){
+            GL15.glBufferData(GL15.GL_ARRAY_BUFFER, objectPoll[i].getMesh(), GL15.GL_STATIC_DRAW);
+        }
+    }
 
 }
